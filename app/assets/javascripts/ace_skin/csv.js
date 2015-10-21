5ada4090292c1d91d9b0dd9c0ecb9d1fec34dc35
panaoke <panaoke@seasonmall.com>
2015-10-21 11:36:50 +0800

    修改内容:       1. 修改富文本控件图片上传支持多张图片一起上传

--- app/assets/javascripts/ace_skin/csv.js
+++ app/assets/javascripts/ace_skin/csv.js
@@ -3,7 +3,9 @@ var Csv = (function() {
     self.getUri = function(data) {
         var csvContent = "data:text/csv;charset=utf-8,";
         data.forEach(function(infoArray, index){
-            dataString = infoArray.join(",");
+            dataString = infoArray.join(',');
+            // 为了实现windows 系统下可以对表格进行正确地拆分, 需要在每行数据末尾添加一个','
+            if(infoArray.length > 0) { dataString += ',' }
             csvContent += index < data.length ? dataString+ "\n" : dataString;
 
         });
