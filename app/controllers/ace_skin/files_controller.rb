da4e5944b283bc390669e57b9e74e1203c16fd58
panaoke <panaoke@seasonmall.com>
2015-12-15 15:09:44 +0800

    修改内容:       1. 阿里云文件上传时, 设置文件的content type属性, 从而实现对svg等其他格式图片的兼容

--- app/controllers/ace_skin/files_controller.rb
+++ app/controllers/ace_skin/files_controller.rb
@@ -5,7 +5,7 @@ module AceSkin
           # render json: {}
         urls = params[:ace_file].map do |file|
           begin
-            AceSkin::Uploader.instance.fast_upload(file.tempfile.path)
+            AceSkin::Uploader.instance.fast_upload(file.tempfile.path, {content_type: file.content_type})
           rescue => e
             next
           end
--- lib/ace_skin/uploader.rb
+++ lib/ace_skin/uploader.rb
@@ -12,12 +12,12 @@ module AceSkin
         aliyun_area: CarrierWave::Uploader::Base.aliyun_area || 'cn-hangzhou' ,
         aliyun_internal: CarrierWave::Uploader::Base.aliyun_internal ,
         aliyun_upload_host: CarrierWave::Uploader::Base.aliyun_upload_host,
+        aliyun_host: CarrierWave::Uploader::Base.aliyun_host,
       })
     end
 
     def fast_upload(file, options = {})
       file = File.open(file) if file.is_a?(String)
-
       file_name = file.path.split('/').last
       file_format = file_name.split('.').last
       path = "#{@fast_upload_path}/#{(Time.now.to_f * 10**6).to_i}.#{file_format}"
